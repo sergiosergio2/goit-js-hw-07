3716d7280793e620ac6e819ad6839105835222e7
sergiosergio2 <84338029+sergiosergio2@users.noreply.github.com>
2021-11-14 18:48:07 +0200

    task 07 part 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+let modal;
+gallery.insertAdjacentHTML('beforeend', creatGalleryToHtml());
+gallery.addEventListener ('click', creatModalImages);
+document.addEventListener ("keydown", closeWithEsc);
+
+function addGalleryItemHtml(item) {
+    return `<div class = "gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image"
+    src="${item.preview}"
+    alt="${item.description}"/>
+    </a>
+    </div>`;
+}
+
+function creatGalleryToHtml () {
+    //console.log(galleryItems);
+    return galleryItems.map(addGalleryItemHtml).join("");
+     
+    
+}
+
+function creatModalImages (evt) {
+    console.log("test");
+    console.log(evt);
+    console.log(evt.currentTarget);
+    if (evt.target.nodeName === "IMG") {
+        modal = basicLightbox.create(`
+        <img width="1400" height="900" src="${evt.path[0].currentSrc}" alt="${evt.path[0].alt}">`);
+        modal.show();
+    }
+    evt.preventDefault();
+}
+function closeWithEsc (evt) {
+    if (evt.code === "Escape" && modal.visible()) {
+        modal.close();
+    }
+}
